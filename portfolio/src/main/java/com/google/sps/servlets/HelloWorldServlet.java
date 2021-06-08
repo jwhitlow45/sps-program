8fca4a1524320e72d20714d2ca685f0bae61cd9e
Jacob Whitlow <jwhitlow45@gmail.com>
2021-06-08 20:35:54 +0000

    feat:add button to display hard-coded message

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -6,13 +6,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 
+  ArrayList<String> myStringList = 
+    new ArrayList<String>(Arrays.asList("\"SQL is just cmd-line Excel.\"",
+                                        "\"We do things not because they are easy, but because we thought they were going to be easy.\"",
+                                        "\"6 hours of debugging can save you 5 minutes of reading documentation.\""));
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println(myStringList.get(0));
   }
 }
